426a692d854aebcded8166b68319247de401acd2
az-m <alisonzmills@gmail.com>
2025-06-08 15:16:05 +0100

    ui-fix: changed full image toggle to button for keyboard functionality. oops.

--- client/src/components/FullScreenToggle.jsx
+++ client/src/components/FullScreenToggle.jsx
@@ -3,14 +3,14 @@ import "./FullScreenToggle.css";
 export default function FullScreenToggle({ fullScreen, set }) {
   return (
     <>
-      <div
+      <button
         className="full-screen-toggle"
         onClick={() => {
           set(!fullScreen);
         }}
       >
         {fullScreen ? "Full Image" : "Full Screen"}
-      </div>
+      </button>
     </>
   );
 }
